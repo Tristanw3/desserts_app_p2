d2001df1462adbafc8086b42ecf073a35eee0a91
mdiedricks <michael.diedricks@gmail.com>
2019-12-18 00:01:11 +1100

    further work to login and log out

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import axios from 'axios';
 import { SummerHome } from './components/Home/SummerHome';
 // import {WinterHome} from './components/Home/WinterHome';
 import { MenuItems } from './components/MenuItems/Menu';
@@ -12,17 +13,88 @@ import { NavigationBar } from './components/NavigationBar';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
+	
+	constructor(props) {
+		super(props);
+		let auth = JSON.parse(sessionStorage.getItem('auth'));
+		this.state = {
+			isLoggedIn: !!auth ? true : false,
+			currentUser: null,
+		}
+	}
+
+	componentDidMount() {
+		this.getUser();
+	}
+
+	getUser() {
+		let auth = JSON.parse(sessionStorage.getItem('auth'));
+		if(!auth) return;
+		axios.get(`/api/users/${auth.userId}`,{ 
+			headers: {"Authorization" : `Bearer ${auth.token}`} 
+		}).then(response => {
+			this.setState({
+				currentUser: response.data, 
+				isLoggedIn: true
+			});
+			return<Redirect to='/' />;
+			
+		});
+	}
+	
+	handleLogin(email, password) {
+		axios.post(`/api/auth/get_token`, {
+			email: email,
+			password: password
+		}).then(response => {
+			sessionStorage.setItem('auth', JSON.stringify(response.data));
+			this.getUser();
+		}).catch(err => {
+			console.log('Failed to login user with error: ');
+			console.log(err)
+		});
+	}
+
+	handleLogout() {
+		sessionStorage.setItem('auth', null);
+		this.setState({currentUser: null, isLoggedIn: false});
+	}
+	
 	render() {
+		const userProps = {
+			isLoggedIn: this.state.isLoggedIn,
+			currentUser: this.state.currentUser,
+			logout: () => this.handleLogout(),
+			login: (email, pass) => this.handleLogin(email, pass)
+		};
+
+		const cartProps = {
+			cartCount: null,
+			addToCart: (item) => this.handleAddToCart(item)
+		}
+
 		return (
 			<React.Fragment>
 				<NavigationBar />
 				<Router>
 					<Switch>
-						<Route exact path="/" component={SummerHome} />
-						<Route path="/menu" component={MenuItems} />
-						<Route path="/purchase" component={Purchase} />
-						<Route path="/login" component={Login} />
-						<Route path="/signup" component={Signup} />
+						<Route exact path="/" component={SummerHome} >
+							<SummerHome user={userProps} cart={cartProps}></SummerHome>
+						</Route>
+						<Route path="/menu" component={MenuItems}>
+							<MenuItems user={userProps} cart={cartProps}></MenuItems>
+						</Route>
+						<Route path="/purchase" component={Purchase}>
+							<Purchase user={userProps} cart={cartProps}></Purchase>
+						</Route>
+						<Route path="/login" component={Login}>
+							<Login user={userProps} cart={cartProps}></Login>
+						</Route>
+						{/* <Route path="/signup" component={Signup} user={userProps} cart={cartProps}/> */}
+						<Route path="/signup">
+							<Signup user={userProps} cart={cartProps}></Signup>
+						</Route>
+
 						<Route component={NoMatch} />
 					</Switch>
 				</Router>
--- web/src/components/Login/Login.js
+++ web/src/components/Login/Login.js
@@ -1,45 +1,36 @@
 import React from 'react';
 import { LoginPage, YellowBanner, PinkContainer, LoginStyled, LoginHeader, AquaBanner } from './LoginStyled';
 import { Button, StyledLink, SignupLink, InputStyling } from '../Button';
-import axios from 'axios';
+
 
 export default class Login extends React.Component {
-	state = {
+	constructor(props){
+		super(props);
+
+		// this.handleLoginBtn = this.handleLoginBtn.bind(this);
+
+		this.state = { 
 		email: '',
-		password: ''
+		password: ''}
 	};
 
-	handleEmail = (e) => {
+	handleEmailForm = (e) => {
 		this.setState({
 			...this.state,
 			email: e.target.value
 		});
 	};
 
-	handlePassword = (e) => {
+	handlePasswordForm = (e) => {
 		this.setState({
 			...this.state,
 			password: e.target.value
 		});
 	};
-	//MICHAEL TO CONTINUE WITH THE BELOW FUNCTION
-	handleLogin = (e) => {
-		const user = {
-			email: this.state.email,
-			password: this.state.password
-		};
-		axios
-			.post('/api/auth/get_token', { email: user.email, password: user.password })
-			.then((resp) => {
-				sessionStorage.setItem('Auth', resp.data.token);
-				sessionStorage.setItem('loggedInStatus', true);
-				sessionStorage.setItem('user', user.email);
-				console.log(sessionStorage);
-			})
-			.catch((err) => {
-				console.log('Failed to login user with error: ');
-				console.log(err);
-			});
+	
+	handleLoginBtn = () =>{
+		console.log(this.props)
+		this.props.user.login(this.state.email, this.state.password)
 	};
 	render() {
 		return (
@@ -50,11 +41,11 @@ export default class Login extends React.Component {
 					<LoginStyled>
 						<LoginHeader>Login</LoginHeader>
 						<InputStyling>
-							<input type="text" placeholder="Email" onChange={this.handleEmail} />
-							<input type="text" placeholder="Password" onChange={this.handlePassword} />
+							<input type="text" placeholder="Email" onChange={this.handleEmailForm} />
+							<input type="text" placeholder="Password" onChange={this.handlePasswordForm} />
 						</InputStyling>
 						<StyledLink to="/menu">
-							<Button onClick={this.handleLogin}>Login</Button>
+							<Button onClick={this.handleLoginBtn}>Login</Button>
 						</StyledLink>
 						<p>
 							<SignupLink to="/signup">Need to Sign-Up?</SignupLink>
